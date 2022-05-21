5b10e162321fbd2dd16f7887a84221fada8f21c4
SMhamidAzad <hamidazad020@gmail.com>
2022-05-21 22:48:52 +0600

    setup tailwind, daiui ui, banner and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ff0000",
+          secondary: "#ff0000",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
