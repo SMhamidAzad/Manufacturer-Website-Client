63543b6ca25f96f34c5f2d84e6b1edb9f79d225d
SMhamidAzad <hamidazad020@gmail.com>
2022-05-24 23:13:19 +0600

    purchase page updated

--- src/components/Pages/Home/Purchase.js
+++ src/components/Pages/Home/Purchase.js
@@ -1,14 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Purchase = () => {
     const { id } = useParams();
     const [tool, setTool] = useState([])
+    // const 
+    const [quantityMessage, setQuantityMessage] = useState({
+        message: ""
+    })
+    const [user, loading, error] = useAuthState(auth);
     useEffect(() => {
         fetch(`http://localhost:5000/tools/${id}`)
             .then(res => res.json())
             .then(data => setTool(data))
     }, [])
+    useEffect(() => {
+        if (user) {
+            console.log(user);
+        }
+    }, [user])
+    const handleQuantityField = e =>{
+         const inputQuantity = parseInt((e.target.value));
+         if(inputQuantity<tool.minimum_order_quantity){
+            setQuantityMessage({...quantityMessage, message: "Quantity should be more than minimum quantity"})
+         }
+         else if(inputQuantity>tool.available_quantity){
+            setQuantityMessage({...quantityMessage, message: "Quantity should be less than available quantity"})
+         }
+         else{
+            setQuantityMessage({...quantityMessage, message: ""})
+         }
+         console.log(quantityMessage.message);
+    }
+    const handleBuyNow = e=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = user?.email;
+        const address = e.target.address.value;
+        const quantity = e.target.quantity.value;
+
+        console.log(name, email, address, quantity);
+    }
     return (
         <div>
             <div className='flex justify-center my-8'>
@@ -24,11 +58,27 @@ const Purchase = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                <h3 className='text-center text-2xl'>Purchase Now</h3>
-                <form action="">
-                    <input className='input input-bordered'  type="email" name="email" id="" placeholder='Email' />
-                </form>
+            <div className='flex justify-center my-10'>
+                <div className=''>
+                    <h3 className='text-2xl mb-4'>Purchase Now</h3>
+                    <form onSubmit={handleBuyNow}>
+                        <input value={user?.email} className='input input-bordered' type="email" name="email" required readOnly disabled/>
+                        <br />
+                        <br />
+                        <input value='user' className='input input-bordered' type="email" name="name" required readOnly disabled/>
+                        <br />
+                        <br />
+                        <input className='input input-bordered' type="address" placeholder='Address' name="address" required/>
+                        <br />
+                        <br />
+                        <p>Maximum Quantity: {tool.available_quantity}</p>
+                        <input className='input input-bordered' type="number" onChange={handleQuantityField} placeholder={tool.minimum_order_quantity} name="quantity" required/>
+                        <br />
+                        {quantityMessage?.message && <p>{quantityMessage.message}</p>}
+                        <br />
+                        <input className='input input-bordered w-full btn-outline' type="submit" name="" value='Buy Now' required/>
+                    </form>
+                </div>
             </div>
         </div>
     );
