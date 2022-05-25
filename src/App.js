129357e0cfaffafbecb623b77b2bccad93673db7
SMhamidAzad <hamidazad020@gmail.com>
2022-05-25 07:48:18 +0600

    requireauth system added for purchase page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Blogs from './components/Pages/Blogs/Blogs';
 import Home from './components/Pages/Home/Home';
 import Purchase from './components/Pages/Home/Purchase';
 import Login from './components/Pages/Login/Login';
+import RequireAuth from './components/Pages/Login/RequireAuth';
 import Portfolio from './components/Pages/Portfolio/Portfolio';
 import Footer from './components/Shared/Footer';
 import Header from './components/Shared/Header';
@@ -15,7 +16,11 @@ function App() {
      <Header></Header>
      <Routes>
        <Route path='/' element={<Home></Home>}></Route>
-       <Route path='/purchase/:id' element={<Purchase></Purchase>}></Route>
+       <Route path='/purchase/:id' element={
+       <RequireAuth>
+         <Purchase></Purchase>
+       </RequireAuth>       
+       }></Route>
        <Route path='/portfolio' element={<Portfolio></Portfolio>}></Route>
        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
        <Route path='/login' element={<Login></Login>}></Route>
